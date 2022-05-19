b0df6fc2f89dc2ee1656d53855c902d29c51d562
Ray_Heng <1987heng@gmail.com>
2022-05-18 20:13:52 -0400

    add method for kitchen
    brainstorm for what kitchen needs.
    1.stove currently available or not.
    2.create a HashMap<string , int> to store the menu and cooking time.

--- VirtualRestaurant/src/virtualrestaurant/Kitchen.java
+++ VirtualRestaurant/src/virtualrestaurant/Kitchen.java
@@ -3,21 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package virtualrestaurant;
-
+import java.util.*;
 /**
  *
- * @author littercow
+ * @author littercow, Cattail
  */
 public class Kitchen {
+    //create a Hashmap to store the dish name and cooking time.
+    HashMap<String, Integer> menu = new HashMap<String, Integer>();
+    String dish;
+    boolean stove = true;
 
-    String timer;
-    String stove;
-
-    public Kitchen(String timer, String stove) {
-        this.timer = timer;
+    public Kitchen(String dish, Integer stove) {
         this.stove = stove;
     }
 
+    //use dish name to find the timer.
+    private int getTimer(String dish){
+        return menu.get(dish);
+    }
+
+    //check method to check if the stove currently open.
+    public boolean isStoveOpen(){
+        return stove;
+    }
+
+    
     public Kitchen() {
     }
 
